b42ab13c9d4aaa95199071783d7d62844246fbde
nishant <nishantpriya.scorpion@gmail.com>
2025-08-02 17:29:58 +0530

    finished

--- app/src/main/java/com/example/mealapp/category.kt
+++ app/src/main/java/com/example/mealapp/category.kt
@@ -1,14 +1,15 @@
 package com.example.mealapp
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class category(
     val idCategory: String,
     val strCategory: String,
     val strCategoryThumb: String,
     val strCategoryDescription: String
-) {
-
-}
-
+): Parcelable
 
 data class CategoriesRes(val cats: List<category>)
 
